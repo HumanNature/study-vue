98f84df55fa4eb6200d1d4c16f402facbebd83c5
HumanNature <1753073774@qq.com>
2021-01-19 11:54:03 +0800

    yes

---
+++ minimail/src/views/home/homechild/HomeSwiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <swiper>
+    <swiper-item v-for="(item, index) in banners" :key="index">
+      <a :href="item.link">
+        <img :src="item.image" alt="" @load="imageLoad" />
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+// import Swiper from 'components/common/swiper/Swiper'
+// import SwiperItem from 'components/common/swiper/SwiperItem'
+// 向上面这样我们可以直接导入
+import { Swiper, SwiperItem } from "components/common/swiper";
+export default {
+  name: "HomeSwiper",
+  props: {
+    banners: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {
+      isLoad: false,
+    };
+  },
+  components: {
+    Swiper,
+    SwiperItem,
+  },
+  methods: {
+    imageLoad() {
+      if (!this.isLoad) {
+        this.$emit("swiperItemLoad");
+        this.isLoad = true;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ minimail/src/views/profile/Profile.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="profile">
+    <nav-bar class="navbar">
+      <div slot="center">我 的</div>
+    </nav-bar>
+    <me-list />
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar.vue";
+import MeList from './mechild/MeList'
+export default {
+  name: "Profile",
+  components: {
+    NavBar,
+    MeList,
+
+  },
+};
+</script>
+
+<style scoped>
+.navbar {
+  background-color: pink;
+  color: #fff;
+  font-weight: bold;
+}
+</style>
